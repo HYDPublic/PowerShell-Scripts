11f442010796d39647e45e43a4a46ee32d237d3e
JamesEarle <j_earle@hotmail.com>
2017-04-11 16:36:55 -0700

    added kill and restart functionality

--- RunDocker.ps1
+++ RunDocker.ps1
@@ -1,3 +1,14 @@
 # Shortcut for running docker script to render techcasestudies markdown
 
-docker run --rm -v C:\Users\jaearle\Documents\Code\techcasestudies-private:/site -p 4000:4000 andredumas/github-pages serve --watch --force_polling
+function startServer() {
+    docker run --rm -v C:\Users\jaearle\Documents\Code\techcasestudies-private:/site -p 4000:4000 andredumas/github-pages serve --watch --force_polling
+}
+
+startServer
+
+# Only executes when the above command fails
+if ($LASTEXITCODE -eq 125) {
+    # The port wasn't correctly deallocated, delete
+    docker kill $(docker ps -q) # NOTE this command kills ALL VMs you have.
+    startServer
+}
