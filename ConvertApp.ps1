7331b499daa4b7b6ff493298af72fff60ca0e621
JamesEarle <j_earle@hotmail.com>
2017-04-11 17:28:45 -0700

    code improvements in ConvertApp

--- ConvertApp.ps1
+++ ConvertApp.ps1
@@ -5,18 +5,10 @@
 # and run this script. This relies on the top level directory being named the same as the AppId
 #
 # TODO run certutil as admin always? 
-
-# If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-
-# {   
-#     $arguments = "& '" + $myinvocation.mycommand.definition + "'"
-#     Start-Process powershell -Verb runAs -ArgumentList $arguments
-#     Break
-# }
+# TODO run makecert silently without private key password (or default none)
 
 function ConvertApp ($name, $publisher) {
     makeappx.exe pack /l /d ".\$name\" /p "$name.appx";
-    
     $val = If ($publisher -ne $null) {$publisher} else {$name}
     makecert.exe -r -h 0 -n "CN=$val" -eku 1.3.6.1.5.5.7.3.3 -pe -sv "$name.pvk" "$name.cer";
 
@@ -28,8 +20,6 @@ function ConvertApp ($name, $publisher) {
 
 if ($args.Length -eq 0) { # Passed nothing.
     Write-Output "Please provide an AppId.";
-} elseif ($args.Length -eq 1) { # Passed an AppId only
-    ConvertApp $args[0];
-} elseif ($args.length -gt 1) { # Passed an AppId and Publisher
-    ConvertApp $args[0] $args[1];
+} else {
+    ConvertApp $args[0] $args[1]
 }
